d84e2f817f75715b0ef0d8484995f59cbf329c12
naldinho <francinaldofranciscoafarias@gmail.com>
2026-04-24 19:52:46 -0300

    primeiro commit

---
+++ src/test/java/fafenterprise/dev/gograduation/GoGraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package fafenterprise.dev.gograduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoGraduationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
