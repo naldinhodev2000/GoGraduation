06f69a4db1a136c7382a980d42da8f6430a8442a
NaldinhoDev <ffranciscoafarias@gmail.com>
2026-04-26 00:30:16 -0300

    mapeando a tabela de usuario

---
+++ src/main/java/fafenterprise/dev/gograduation/entity/UsuarioEntity.java
@@ -0,0 +1,50 @@
+package fafenterprise.dev.gograduation.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+
+public class UsuarioEntity {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotBlank
+    @Column (name = "nome", nullable = false)
+    private String nome;
+
+    @NotBlank
+    @Column (name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(name = "telefone", nullable = false, unique = true)
+    private String telefone;
+
+    @NotBlank
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @NotBlank
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @Column(name = "data_cadastro", nullable = false)
+    private LocalDateTime dataCadastro;
+
+    @Column(name = "data_atualizacao", nullable = false)
+    private LocalDateTime dataAtualizacao;
+
+}
