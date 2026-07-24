78e4704d9e7332f4aa6857187a6260707ba921fd
Naldinho <ffranciscoafarias@gmail.com>
2026-07-24 15:58:21 -0300

    atualizando saldo

--- backend/src/main/java/fafenterprise/dev/gograduation/entity/uno/Cash.java
+++ backend/src/main/java/fafenterprise/dev/gograduation/entity/uno/Cash.java
@@ -6,13 +6,9 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
